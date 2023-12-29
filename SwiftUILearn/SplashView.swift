303a0f4c27ceed11cbaac5edaefc91ecd09ecc49
krishna <33350985+kushwaha03@users.noreply.github.com>
2023-12-29 12:15:33 +0530

    added splash screen

---
+++ SwiftUILearn/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  SwiftUILearn
+//
+//  Created by Radhe on 29/12/23.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
