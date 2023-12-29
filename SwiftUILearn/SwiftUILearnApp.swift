16ae7d6cf4454c8ebec799329ce6327e0cd66ecf
krishna <33350985+kushwaha03@users.noreply.github.com>
2023-12-29 11:56:51 +0530

    Initial Commit

---
+++ SwiftUILearn/SwiftUILearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUILearnApp.swift
+//  SwiftUILearn
+//
+//  Created by Radhe on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUILearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
